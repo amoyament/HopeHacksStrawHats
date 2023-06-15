74f815e0acd776e554e533dd5ac9b5200b4b2d4c
sodoeme <sodoeme@gmail.com>
2023-06-15 16:42:57 -0400

    proj skeleton2

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ connection.connect(function (err) {
     if (err) {
       return console.error("error: " + err.message);
     }
-  
+   
     let createUser = `CREATE TABLE IF NOT EXISTS users (
         id INT PRIMARY KEY AUTO_INCREMENT,
         firstName VARCHAR(255) NOT NULL,
